e458852c76399fa25f6f0c213323ed407313bb5b
unknown <cristian>
2022-04-15 13:49:13 -0300

    Falta los ajustes finales

--- sistema/urls.py
+++ sistema/urls.py
@@ -7,8 +7,10 @@ urlpatterns = [
     path('',views.inicio,name='inicio'),
     path("libros", views.libros,name='libros'),
     path('nosotros',views.nosotros,name='nosotros'),
-    path('crear',views.crear,name='crear'),
-    path('editar',views.crear,name='editar'),
+    path('libros/crear',views.crear,name='crear'),
+    path('libros/editar',views.crear,name='editar'),
+    path('eliminar/<int:id>',views.eliminar,name='eliminar'),
+    path('libros/editar/<int:id>',views.editar,name='editar'),
     
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 # 50:16
